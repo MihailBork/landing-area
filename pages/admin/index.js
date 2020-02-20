abac918f2248731c2de1018b1910fbe0af64edcb
Mikhail Kutnyashenko <mikhail@effective.band>
2020-02-20 12:40:16 +0300

    Created a template pages

---
+++ pages/admin/index.js
@@ -0,0 +1,36 @@
+import Head from 'next/head';
+import Router from 'next/router';
+import { parseCookies } from 'nookies';
+import _ from 'lodash';
+
+const Admin = () => (
+    <div className="container">
+        <Head>
+            <title>Admin page</title>
+            <link rel="icon" href="/favicon.ico" />
+        </Head>
+
+        <main>
+            <h1 className="title">
+                Welcome to admin panel!
+            </h1>
+
+            <p className="description">
+                This is protected route
+            </p>
+        </main>
+    </div>
+);
+
+Admin.getInitialProps = async (ctx) => {
+    const cookies = parseCookies(ctx);
+    if (!_.get(cookies, `profile`)) {
+        ctx.res.writeHead(302, {
+            Location: '/auth'
+        })
+        ctx.res.end();
+    }
+    return {}
+};
+
+export default Admin
---
+++ pages/auth/index.js
@@ -0,0 +1,25 @@
+import Head from 'next/head';
+import Router from 'next/router';
+import { parseCookies } from 'nookies';
+import _ from 'lodash';
+
+const Auth = () => (
+    <div className="container">
+        <Head>
+            <title>Authorization page</title>
+            <link rel="icon" href="/favicon.ico" />
+        </Head>
+
+        <main>
+            <h1 className="title">
+                Need to authorize here
+            </h1>
+
+            <p className="description">
+                This is public route
+            </p>
+        </main>
+    </div>
+);
+
+export default Auth
