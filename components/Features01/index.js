824f222452bf9a29daa1d9f3a8f2f872634139ec
Mikhail Kutnyashenko <mikhail@effective.band>
2020-02-25 21:04:26 +0300

    Added Feature01 component

---
+++ components/Features01/index.js
@@ -0,0 +1,145 @@
+import React, {useState, useEffect} from "react";
+import b_ from 'b_';
+import cn from 'classnames';
+import {motion} from 'framer-motion';
+
+import VisibilitySensor from 'react-visibility-sensor';
+
+import './style.scss';
+
+const features = [
+    {
+        description: [
+            {
+                number: 5,
+                text: `недель обучения`,
+            }
+        ],
+        icon: `calendar`
+    },
+    {
+        description: [
+            {
+                number: 13600,
+                text: `по предоплате`,
+            },
+            {
+                number: 17000,
+                text: `в день старта`,
+            }
+        ],
+        icon: `ruble`
+    },
+    {
+        description: [
+            {
+                number: 45,
+                text: `часов теории`,
+            },
+            {
+                number: 10,
+                text: `часов практики`,
+            }
+        ],
+        icon: `clock`
+    }
+];
+
+const paragraphs = [
+    `Разнообразный и богатый опыт постоянное информационно-пропагандистское обеспечение нашей деятельности в значительной степени обуславливает создание систем массового участия.`,
+    `Не следует, однако забывать, что постоянный количественный рост и сфера нашей активности влечет за собой процесс внедрения и модернизации модели развития. Значимость этих проблем настолько очевидна, что постоянное информационно-пропагандистское обеспечение нашей деятельности в значительной степени обуславливает создание направлений прогрессивного развития.`,
+    `Не следует, однако забывать, что постоянный количественный рост и сфера нашей активности требуют от нас анализа системы обучения кадров, соответствует насущным потребностям. Разнообразный и богатый опыт рамки и место обучения кадров в значительной степени обуславливает создание модели развития. Не следует, однако забывать, что рамки и место обучения кадров в значительной степени обуславливает создание соответствующий условий активизации.`
+];
+
+export const b = b_.lock(`Features01`);
+
+const Paragraph = ({ item, pageMoveValue }) => {
+    const [isShowed, setShowState] = useState(false);
+    const onVisibilityChange = (isVisible) => {
+        if (!isVisible || isShowed) return;
+        setShowState(true);
+    }
+    return (
+        <VisibilitySensor onChange={onVisibilityChange} partialVisibility={true}>
+            <div className={b(`paragraphs-item`)}>
+                <motion.div
+                    animate={{
+                        x: isShowed ? pageMoveValue : 0,
+                        transition: {
+                            duration: 2,
+                        }
+                    }}
+                    className={cn(b(`paragraphs-item-gate`), `left`)}
+                />
+                <motion.div
+                    animate={{
+                        x: isShowed ? -pageMoveValue : 0,
+                        transition: {
+                            duration: 2,
+                        }
+                    }}
+                    className={cn(b(`paragraphs-item-gate`), `right`)}
+                />
+                <p>
+                    {item}
+                </p>
+            </div>
+        </VisibilitySensor>
+    )
+};
+
+const Features01 = () => {
+    const [width, setWidth] = useState(0);
+    useEffect(() => {
+        const updateSize = () => {
+            if (!process.browser) return;
+            setWidth(window.innerWidth);
+        };
+        window.addEventListener('resize', updateSize);
+        updateSize();
+        return () => window.removeEventListener('resize', updateSize);
+    }, []);
+    const pageMoveValue = -(width / 2) + (width > 800 ? 30 : 20);
+    return (
+        <div className={cn(b(), `ComponentWrapper`)}>
+            <div className={b(`title`)}>
+                <h1>Обучающая программа</h1>
+                <h2>с практикой от маркетологов из MyAcademy</h2>
+            </div>
+            <div className={b(`items`)}>
+                {
+                    features.map((item, index) => (
+                        <div key={index} className={b(`items-column`)}>
+                            <div className={b(`items-column-icon`)}>
+                                <img className={b(`items-column-icon-image`)}
+                                     src={`/images/Features01/${item.icon}.svg`}/>
+                                <img className={cn(b(`items-column-icon-border`), `item-${index}`)}
+                                     src={`/images/Features01/round-black.png`}/>
+                            </div>
+                            <div className={b(`items-column-description`)}>
+                                {
+                                    item.description.map((point, i) => (
+                                        <div key={i} className={b(`items-column-description-row`)}>
+                                            <div
+                                                className={b(`items-column-description-row-number`)}>{point.number}</div>
+                                            <div className={b(`items-column-description-row-text`)}>{point.text}</div>
+                                        </div>
+                                    ))
+                                }
+                            </div>
+                        </div>
+                    ))
+                }
+            </div>
+            <div className={b(`paragraphs`)}>
+                {
+                    paragraphs.map((item, index) => (
+                        <Paragraph key={index} item={item} pageMoveValue={pageMoveValue} />
+                    ))
+                }
+            </div>
+        </div>
+    )
+};
+
+export default Features01;
--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
 import React from "react";
 import Title01 from "../components/Title01";
 import List01 from "../components/List01";
+import Features01 from "../components/Features01";
 
 import './style.scss';
 
@@ -9,6 +10,7 @@ const Home = () => (
   <div className="container">
       <Title01 />
       <List01 />
+      <Features01 />
   </div>
 );
 
