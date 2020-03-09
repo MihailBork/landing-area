0398979abbd68d776170ffc404d97da70469bb03
Mikhail Kutnyashenko <mikhail@effective.band>
2020-03-09 22:55:29 +0300

    Fixed mistake in api const

--- api/index.js
+++ api/index.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const dev = process.env.NODE_END !== 'production';
+const dev = process.env.NODE_ENV !== 'production';
 
 export const api = axios.create({
     baseURL: dev ? `http://localhost:3001` : `http://ayta.thebork.ru:3001`,
