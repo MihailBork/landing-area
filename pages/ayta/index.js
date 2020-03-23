dc4cd36a62f636b427b2882d197832c7afb9207f
Mikhail Kutnyashenko <mikhail@effective.band>
2020-03-23 19:11:09 +0300

    Update paths and add unversioned files

--- pages/ayta/index.js
+++ pages/ayta/index.js
@@ -1,21 +1,18 @@
 import React, {useState, useEffect} from "react";
 import Head from 'next/head';
-import Title01 from "../components/Title01";
-import Title02 from "../components/Title02";
-import List01 from "../components/List01";
-import Features01 from "../components/Features01";
-import Features02 from "../components/Features02";
-import Person01 from "../components/Person01";
-import Columns01 from "../components/Columns01";
-import About01 from "../components/About01";
-import Form01 from "../components/Form01";
-import Copyright01 from "../components/Copyright01";
-import Menu01 from "../components/Menu01";
-import Clients01 from "../components/Clients01";
-import Gallery01 from "../components/Gallery01";
-import Works01 from "../components/Works01";
+import Title01 from "../../components/Title01";
+import List01 from "../../components/List01";
+import Features01 from "../../components/Features01";
+import Features02 from "../../components/Features02";
+import Person01 from "../../components/Person01";
+import Columns01 from "../../components/Columns01";
+import About01 from "../../components/About01";
+import Form01 from "../../components/Form01";
+import Copyright01 from "../../components/Copyright01";
+import Menu01 from "../../components/Menu01";
+import Clients01 from "../../components/Clients01";
 
-import './style.scss';
+import '../style.scss';
 
 const Home = () => {
     const [firstTimeLoading, setFirstTimeLoadingState] = useState(true);
