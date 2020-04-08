0c29f4908e8ecdf4aa1c0e953120d0fec84edfc3
Mikhail Kutnyashenko <mikhail@effective.band>
2020-04-08 15:23:34 +0300

    Changed eslint loading in next config

--- next.config.js
+++ next.config.js
@@ -2,18 +2,20 @@ const withSass = require('@zeit/next-sass');
 const path = require(`path`);
 
 module.exports = withSass({
-  webpack(config, {dev}) {
-    config.module.rules.push({
-      test: /\.js$/,
-      enforce: `pre`,
-      exclude: /node_modules/,
-      loader: `eslint-loader`,
-      options: {
-        // Emit errors as warnings for dev to not break webpack build.
-        // Eslint errors are shown in console for dev, yay :-)
-        emitWarning: dev,
-      },
-    })
+  webpack(config, { dev }) {
+    if (dev) {
+      config.module.rules.push({
+        test: /\.js$/,
+        enforce: `pre`,
+        exclude: /node_modules/,
+        loader: `eslint-loader`,
+        options: {
+          // Emit errors as warnings for dev to not break webpack build.
+          // Eslint errors are shown in console for dev, yay :-)
+          emitWarning: dev,
+        },
+      })
+    }
 
     config.resolve.modules.push(path.resolve('./'))
     return config
