dd6064454c85f170353399e481fb55d3a0401636
Mikhail Kutnyashenko <mikhail@effective.band>
2020-04-08 17:08:05 +0300

    Fixed code style

--- server/controllers/kotelnikovo.controller.js
+++ server/controllers/kotelnikovo.controller.js
@@ -1,159 +1,152 @@
-const {addWork, getWorks} = require('../db/kotelnikovo/works.model');
-const _ = require('lodash');
-const fs = require('fs');
-const path = require('path');
+const { addWork, getWorks } = require(`../db/kotelnikovo/works.model`);
+const _ = require(`lodash`);
+const fs = require(`fs`);
+const path = require(`path`);
 
-const multer = require('multer');
-const storage = multer.diskStorage({
-    destination: function (req, file, cb) {
-        cb(null, '../uploads/')
-    },
-    filename: function (req, file, cb) {
-        crypto.randomBytes(16, function (err, raw) {
-            if (err) return cb(err)
+const multer = require(`multer`);
 
-            cb(null, raw.toString('hex') + path.extname(file.originalname))
-        })
-    }
-});
 const upload = multer({
-    storage: multer.memoryStorage(),
-    limits: {
-        fieldNameSize: 255,
-        fileSize: 1 * 1024 * 1024, // 1Mb
-    }
-}).fields([{name: `photo`, maxCount: 1}, {name: `work`, maxCount: 1}]);
+  storage: multer.memoryStorage(),
+  limits: {
+    fieldNameSize: 255,
+    fileSize: 1 * 1024 * 1024, // 1Mb
+  },
+})
+  .fields([{
+    name: `photo`,
+    maxCount: 1,
+  }, {
+    name: `work`,
+    maxCount: 1,
+  }]);
 
-const getFile = (files, name) => {
-    return _.first(_.get(files, name));
-};
+const getFile = (files, name) => _.first(_.get(files, name));
 
 const checkFileLimits = ({ file, maxSize, availableTypes }) => {
-    const fileSize = _.get(file, `size`);
-    const fileType = _.get(file, `mimetype`);
-    const fileName = _.get(file, `originalname`);
+  const fileSize = _.get(file, `size`);
+  const fileType = _.get(file, `mimetype`);
+  const fileName = _.get(file, `originalname`);
 
-    if (!_.includes(availableTypes, fileType)) {
-        return `Недопустимое расширение файла ${fileName}`;
-    }
+  if (!_.includes(availableTypes, fileType)) {
+    return `Недопустимое расширение файла ${fileName}`;
+  }
 
-    if (fileSize > maxSize) {
-        return `Превышен максимальный размер файла ${fileName}`;
-    }
+  if (fileSize > maxSize) {
+    return `Превышен максимальный размер файла ${fileName}`;
+  }
 
-    return null;
+  return null;
 };
 
-const errorResponse = (res, message) => {
-    return res.status(400).json({
-        ok: false,
-        message,
-    });
-};
+const errorResponse = (res, message) => res.status(400).json({ ok: false, message });
 
 const uploadFile = (file) => {
-    const buffer = _.get(file, `buffer`);
-    const fileName = `${file.fieldname}-${Date.now()}${path.extname(file.originalname)}`;
-    const wstream = fs.createWriteStream(`./public/uploads/${fileName}`);
-    wstream.write(buffer);
-    return fileName;
+  const buffer = _.get(file, `buffer`);
+  const fileName = `${file.fieldname}-${Date.now()}${path.extname(file.originalname)}`;
+  const wstream = fs.createWriteStream(`./public/uploads/${fileName}`);
+  wstream.write(buffer);
+  return fileName;
 };
 
 module.exports = {
-    uploadWork: function (req, res) {
-        upload(req, res, err => {
-            if (err) {
-                errorResponse(res, `Ошибка загрузки файлов: ${_.get(err, `message`, `неопознанная ошибка`)}`);
-                return;
-            }
+  uploadWork: (req, res) => {
+    upload(req, res, (err) => {
+      if (err) {
+        errorResponse(res, `Ошибка загрузки файлов: ${_.get(err, `message`, `неопознанная ошибка`)}`);
+        return;
+      }
 
-            const {name, about} = req.body;
-            if (!name || !about) {
-                errorResponse(res, `Не заполнены обязательные поля`);
-                return;
-            }
+      const { name, about } = req.body;
+      if (!name || !about) {
+        errorResponse(res, `Не заполнены обязательные поля`);
+        return;
+      }
 
-            const photoFile = getFile(_.get(req, `files`), `photo`);
-            const workFile = getFile(_.get(req, `files`), `work`);
+      const photoFile = getFile(_.get(req, `files`), `photo`);
+      const workFile = getFile(_.get(req, `files`), `work`);
 
-            if (!photoFile || !workFile) {
-                errorResponse(res, `Отсутствует один или несколько файлов`);
-                return;
-            }
+      if (!photoFile || !workFile) {
+        errorResponse(res, `Отсутствует один или несколько файлов`);
+        return;
+      }
 
-            const photoFileErrors = checkFileLimits({
-                file: photoFile,
-                maxSize: 1 * 1024 * 1024,
-                availableTypes: [`image/jpeg`, `image/pjpeg`, `image/png`],
-            });
+      const photoFileErrors = checkFileLimits({
+        file: photoFile,
+        maxSize: 1 * 1024 * 1024,
+        availableTypes: [`image/jpeg`, `image/pjpeg`, `image/png`],
+      });
 
-            if (photoFileErrors) {
-                errorResponse(res, photoFileErrors);
-                return;
-            }
+      if (photoFileErrors) {
+        errorResponse(res, photoFileErrors);
+        return;
+      }
 
-            const workFileErrors = checkFileLimits({
-                file: workFile,
-                maxSize: 1 * 1024 * 1024,
-                availableTypes: [`application/pdf`],
-            });
+      const workFileErrors = checkFileLimits({
+        file: workFile,
+        maxSize: 1 * 1024 * 1024,
+        availableTypes: [`application/pdf`],
+      });
 
-            if (workFileErrors) {
-                errorResponse(res, workFileErrors);
-                return;
-            }
+      if (workFileErrors) {
+        errorResponse(res, workFileErrors);
+        return;
+      }
 
-            const photoFileName = uploadFile(photoFile);
-            const workFileName = uploadFile(workFile);
+      const photoFileName = uploadFile(photoFile);
+      const workFileName = uploadFile(workFile);
 
-            addWork({
+      addWork({
+        name,
+        about,
+        photo: photoFileName,
+        work: workFileName,
+      })
+        .then(() => {
+          res.status(200)
+            .json({
+              ok: true,
+              message: `Upload successful`,
+              data: {
                 name,
                 about,
-                photo: photoFileName,
-                work: workFileName,
-            })
-                .then(response => {
-                    res.status(200).json({
-                        ok: true,
-                        message: `Upload successful`,
-                        data: {
-                            name,
-                            about,
-                            files: [
-                                {
-                                    type: `photo`,
-                                    filename: photoFileName,
-                                },
-                                {
-                                    type: `work`,
-                                    filename: workFileName,
-                                }
-                            ],
-                        }
-                    })
-                })
-                .catch(error => {
-                    console.log(error);
-                    errorResponse(res, `Ошибка во время сохранения работы`);
-                });
+                files: [
+                  {
+                    type: `photo`,
+                    filename: photoFileName,
+                  },
+                  {
+                    type: `work`,
+                    filename: workFileName,
+                  },
+                ],
+              },
+            });
         })
-    },
-    getWorks: (req, res) => {
-        getWorks()
-            .then(response => {
-                res.status(200).json({
-                    ok: true,
-                    message: `Success`,
-                    data: response,
-                })
-            })
-            .catch(error => {
-                console.log(error);
-                res.status(400).json({
-                    ok: false,
-                    message: _.get(error, `response`, `Uncaught error`),
-                    data: [],
-                })
-            })
-    }
+        .catch((error) => {
+          // eslint-disable-next-line no-console
+          console.log(error);
+          errorResponse(res, `Ошибка во время сохранения работы`);
+        });
+    });
+  },
+  getWorks: (req, res) => {
+    getWorks()
+      .then((response) => {
+        res.status(200)
+          .json({
+            ok: true,
+            message: `Success`,
+            data: response,
+          });
+      })
+      .catch((error) => {
+        console.log(error);
+        res.status(400)
+          .json({
+            ok: false,
+            message: _.get(error, `response`, `Uncaught error`),
+            data: [],
+          });
+      });
+  },
 };
--- server/routes/kotelnikovo/index.js
+++ server/routes/kotelnikovo/index.js
@@ -1,7 +1,7 @@
-const router = require('express').Router();
-const kotelnikovoController = require('../../controllers/kotelnikovo.controller');
-const crypto = require('crypto');
-const path = require('path');
+const router = require(`express`).Router();
+const kotelnikovoController = require(`../../controllers/kotelnikovo.controller`);
+const crypto = require(`crypto`);
+const path = require(`path`);
 
 router.post('/add', kotelnikovoController.uploadWork);
 router.get('/getWorks', kotelnikovoController.getWorks);
